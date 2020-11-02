a13ada4a66db9343a1164555776c3e19d274266e
Izaird Mothelet <iza_alex3@hotmail.com>
2020-11-02 12:44:26 -0600

    Creation of the gui client side

--- practica_01/client/client.py
+++ practica_01/client/client.py
@@ -1,34 +1,52 @@
 from tkinter import *
-from PIL import ImageTk, Image
+from tkinter import filedialog
 import socket 
 import threading
+import time
+import pickle
 
+def download():
+    s.send(b'1')
 
+def upload():
+    s.send(b'2')
 
-def thread_interface():
-    root = Tk()
-    root.title('Servidor archivos')
-    root.mainloop()
+def delete():
+    s.send(b'3')
 
-
-def thread_socket():
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.connect((socket.gethostname(), 5001))
-    full_msg = ''
-    while True:
-        msg = s.recv(8)
-        if len(msg) <= 0:
-            break
-    full_msg += msg.decode("utf-8")
-    print(msg.decode("utf-8"))
-
-
-
+def update():
+    s.send(b"4")
 
 if __name__ == "__main__":
-    interface = threading.Thread(target=thread_interface)
-    interface.start()
-    socket_server = threading.Thread(target = thread_socket)
-    socket_server.start()
-    print("xd")
+    root = Tk()
+    root.title('Cliente')
+    root.geometry("530x800")
+
+
+    # Button to download files from the server
+    downloandButton = Button(root, text = "Download",padx= 30, pady= 10, command=download)
+    downloandButton.grid(column=0, row=0, padx = 10, pady =5)
+
+    # Button to upload files to the server
+    uploadButton = Button(root, text = "Upload",padx= 30, pady= 10, command=upload)
+    uploadButton.grid(column=1, row=0, padx = 10, pady = 5)
+
+    # Button to delete files from the server
+    deleteButton = Button(root, text = "Delete",padx= 30, pady= 10, command=delete)
+    deleteButton.grid(column=2, row=0, padx=10, pady=5)
+
+    # Button to update info from the server
+    updateButton = Button(root, text = "Update",padx= 30, pady= 10, command=update)
+    updateButton.grid(column=3, row=0, padx=10, pady=5)
+
+    # List of the files and dyrectories on the server
+    listServerFiles = Listbox(root, width=63, height=45)
+    listServerFiles.grid(column=0, row=1, columnspan=4, pady= 10)
+
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.connect((socket.gethostname(), 5000))
+
+
+    root.mainloop()
--- practica_01/server/server.py
+++ practica_01/server/server.py
@@ -1,61 +1,31 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import socket 
-import threading
 from os import listdir, walk 
 from pathlib import Path
-
-def thread_interface():
-    root = Tk()
-    root.title('Servidor archivos')
-    root.geometry("800x600")
-
-
-    def updateFileList():
-        file_names = []
-        directory_names = [] 
-
-
-        for (dirpath, dirnames, filenames) in walk(mypath):
-            file_names.extend(filenames)
-            break        
-
-        for (dirpath, dirnames, filenames) in walk(mypath):
-            directory_names.extend(dirnames)
-
-        listFilesDir = Listbox(root)
-        listFilesDir.grid(column = 0, row = 0)
-        listFilesDir.delete(0, 'END')
-        for x in directory_names:
-            listFilesDir.insert(END,x)
-        for x in file_names:
-            listFilesDir.insert(END,x)
-
-    updateFileList()
-    updateButton = Button(root, text = "actualizar", command = updateFileList)
-    updateButton.grid(column=0, row = 1)
-
-
-    root.mainloop()
-
-
-def thread_socket():
-    PORT = 5001
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.bind((socket.gethostname(),PORT))
-    s.listen()
-    while True:
-        clientsocket, address = s.accept()
-        print(f"Connection from {clientsocket.getpeername()} : {clientsocket.getpeername()}")
-        clientsocket.send(bytes("Welcome to the server!", "utf-8"))
-        clientsocket.close()
 
 
 
 if __name__ == "__main__":
-    mypath = "Files/"
-    interface = threading.Thread(target=thread_interface)
-    interface.start()
-    socket_server = threading.Thread(target = thread_socket)
-    socket_server.start()
+    PORT = 5000
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((socket.gethostname(),PORT))
+    s.listen()
+    print("Waiting for clients ...")
+    while True:
+        clientsocket, address = s.accept()
+        with clientsocket:
+            print(f"Connection from {clientsocket.getpeername()[0]} : {clientsocket.getpeername()[1]}")
+            while True:
+                data = clientsocket.recv(1024)
+                data = int(data)
+                if(data==1):
+                    print(1)
+                elif(data==2):
+                    print(2)
+                elif(data==3):
+                    print(3)
+                elif(data==4):
+                    print("Conection finished")
+                    break
