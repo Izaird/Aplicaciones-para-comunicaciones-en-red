0ff073b130c9a6adfbffa1c42538d348a3781133
Izaird Mothelet <iza_alex3@hotmail.com>
2020-11-02 20:16:32 -0600

    Added functions to: 	-Delete files from the server 	-Update content list from the server

--- practica_01/client/client.py
+++ practica_01/client/client.py
@@ -5,22 +5,66 @@ import threading
 import time
 import pickle
 
+#Function to download a files from the server
 def download():
     s.send(b'1')
 
+#Function to upload a file to the server
 def upload():
+    files = filedialog.askopenfilenames(initialdir="~/Documents",\
+        title="Select the files you want to upload", \
+        filetypes=(("All Files", "*.*"), ("Png", "*.png"))) 
+    print (root.tk.splitlist(files))
     s.send(b'2')
+    data = s.recv(1024)
+    print(data.decode('utf-8'))
 
+#Function to delete a file from the server
 def delete():
-    s.send(b'3')
+    reslist = list()
+    seleccion = listServerFiles.curselection()
+
+    for i in seleccion:
+        entrada = listServerFiles.get(i)
+        reslist.append(entrada)
+    print(reslist)
+
+    #If the user doesn't pick any item then we won't sent anything
+    if not reslist:
+        print("You need to select at least an item")
+    else:
+        s.send(b'3')
+        time.sleep(.05)
+        reslist = pickle.dumps(reslist)
+        s.send(reslist)
+        drive_content = s.recv(1024)
+        drive_content = pickle.loads(drive_content)
+        updateFileList(drive_content)
+    
 
 def update():
     s.send(b"4")
+    drive_content = s.recv(1024)
+    drive_content = pickle.loads(drive_content)
+    print(drive_content)
+    updateFileList(drive_content)
+
+
+def quitApp():
+    s.send(b'5')
+    root.quit()
+
+#Function to update the listbox it needs the list of the files
+def updateFileList(drive_content):
+    listServerFiles.delete(0,END)
+    for x in drive_content:
+        listServerFiles.insert(END,x)
+
 
 if __name__ == "__main__":
     root = Tk()
-    root.title('Cliente')
-    root.geometry("530x800")
+    root.title('Client')
+    root.geometry("640x800")
 
 
     # Button to download files from the server
@@ -39,14 +83,21 @@ if __name__ == "__main__":
     updateButton = Button(root, text = "Update",padx= 30, pady= 10, command=update)
     updateButton.grid(column=3, row=0, padx=10, pady=5)
 
+    # Button to exit the application and end the connection to the server
+    quitButton = Button(root, text="Quit", padx=30, pady=10, command=quitApp)
+    quitButton.grid(column=4, row=0, padx=10, pady=5)
+
     # List of the files and dyrectories on the server
-    listServerFiles = Listbox(root, width=63, height=45)
-    listServerFiles.grid(column=0, row=1, columnspan=4, pady= 10)
+    listServerFiles = Listbox(root, width=75, height=45, selectmode='multiple')
+    listServerFiles.grid(column=0, row=1, columnspan=5, pady= 10)
 
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.connect((socket.gethostname(), 5000))
 
-
+    drive_content = s.recv(1024)
+    drive_content = pickle.loads(drive_content)
+    print(drive_content)
+    updateFileList(drive_content)
     root.mainloop()
--- practica_01/server/server.py
+++ practica_01/server/server.py
@@ -1,13 +1,25 @@
 from tkinter import *
+import pickle
 from PIL import ImageTk, Image
-import socket 
-from os import listdir, walk 
+import socket
+from os import listdir, walk, remove
 from pathlib import Path
+mypath="Files/"
+PORT=5000
 
+# Function to get all the content of the server folder File
+# *It return the information in form of bytes
+def updateFileList():
+    drive_content=[]
+    for(dirpath, dirnames, filenames) in walk(mypath):
+        drive_content.extend(dirnames)
+        drive_content.extend(filenames)
+        break
+    drive_content = pickle.dumps(drive_content)
+    return drive_content
 
 
 if __name__ == "__main__":
-    PORT = 5000
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((socket.gethostname(),PORT))
@@ -16,16 +28,53 @@ if __name__ == "__main__":
     while True:
         clientsocket, address = s.accept()
         with clientsocket:
+
+            #Every time that connect with a new client update the list of files
+            drive_content= updateFileList()
+            #And also send this information to the client
+            clientsocket.send(drive_content)
+
+            #Ip and port of the client
             print(f"Connection from {clientsocket.getpeername()[0]} : {clientsocket.getpeername()[1]}")
+
+            #Once we stablish connection with the client we stay at a infinite loop
+            #waiting for instructions
             while True:
                 data = clientsocket.recv(1024)
+
+                #If there is not response from the cliente then we break out of the loop
+                if not data:
+                    print("ay LMAO")
+                    break
                 data = int(data)
                 if(data==1):
                     print(1)
+
+               #Upload file 
                 elif(data==2):
-                    print(2)
+                    print("The request to upload the files has been received")
+                    clientsocket.send(b"Send the file/files")
+                
+                #Delete files
                 elif(data==3):
-                    print(3)
+                    print("The request to delete the following files has been received:")
+                    delete_this_items = clientsocket.recv(1024)
+                    delete_this_items = pickle.loads(delete_this_items)
+                    for item in delete_this_items:
+                        print(item)
+                        full_path = mypath + item
+                        remove(full_path)
+                    #After deleting the files the server sents an content update
+                    print("Sending updated content list...")
+                    drive_content = updateFileList()
+                    clientsocket.send(drive_content)
+
+                #We send an update about the file list to the client
                 elif(data==4):
+                    print("The request to update the file list has been received")
+                    drive_content = updateFileList()
+                    clientsocket.send(drive_content)
+
+                elif(data==5):
                     print("Conection finished")
                     break
