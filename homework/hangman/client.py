f78ef04801369dd3955fe2cf010fa6f95a6d506c
Izaird Mothelet <iza_alex3@hotmail.com>
2020-11-07 15:41:18 -0600

    added window to choose difficulty

--- homework/hangman/client.py
+++ homework/hangman/client.py
@@ -1,7 +1,52 @@
+import time
 import socket
 from tkinter import *
 PORT = 5000
 HOST = "127.0.0.1"
+
+def difficultyWindow():
+    top = Toplevel()
+    top.title('Chose a difficulty')
+
+    def easyDifficulty():
+        s.send(b'1')
+        time.sleep(0.05)
+        print("easy difficulty choosed ...")
+        s.send(b'easy')
+        time.sleep(0.05)
+        word_len = int(s.recv(1024).decode())
+        print(word_len)
+        # top.destroy()
+
+    def mediumDifficulty():
+        s.send(b'1')
+        time.sleep(0.05)
+        print("medium difficulty choosed ...")
+        s.send(b'medi')
+        time.sleep(0.05)
+        word_len = int(s.recv(1024).decode())
+        print(word_len)
+        # top.destroy()
+
+    def hardDifficulty():
+        s.send(b'1')
+        time.sleep(0.05)
+        print("hard difficulty choosed ...")
+        s.send(b'hard')
+        time.sleep(0.05)
+        word_len = int(s.recv(1024).decode())
+        print(word_len)
+        # top.destroy()
+
+    easy_b = Button(top, text="Easy", padx=30, pady=10, command=easyDifficulty)
+    easy_b.grid(column=0, row=0, padx=10, pady=5)
+
+    medium_b = Button(top, text="Medium", padx=30, pady=10, command=mediumDifficulty)
+    medium_b.grid(column=0, row=1, padx=10, pady=5)
+
+    hard_b = Button(top, text="Hard", padx=30, pady=10, command=hardDifficulty)
+    hard_b.grid(column=0, row=2, padx=10, pady=5)
+
 
 if __name__ == "__main__":
 
@@ -9,8 +54,9 @@ if __name__ == "__main__":
     root.title('Hang man')
     root.geometry('600x800')
 
-    top = Toplevel()
-    top.title('Chose a difficulty')
+
+    difficultyWindow()    
+
 
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- homework/hangman/server.py
+++ homework/hangman/server.py
@@ -1,10 +1,59 @@
-
 import socket
-from tkinter import *
+import random
+import time
 HOST = "127.0.0.1"
 PORT = 5000
 
 if __name__ == "__main__":
+    hard_words = ["hipopotomostrosesquipedaliofobia"\
+                    ,"parangaricutirimicuaro"\
+                    ,"superescalifragilisticoespialidos"\
+                    ,"hexakosioihexekontahexafobia"\
+                    ,"desanparangaricutirimicuarizador"\
+                    ,"paralelepipedos"\
+                    ,"zsoltbaumgartnerismo"\
+                    ,"esternocleidomastoideo"\
+                    ,"homopedonecrozoofilico"\
+                    ,"neotromponolomeonilopolonouta"\
+                    ,"dihidroxifenilalaninasa"\
+                    ,"ugarterragaicoecheapaturri"\
+                    ,"metilendioximetanfetamina"\
+                    ,"caravincuntincuadrado"\
+                    ,"octangolonoplasentaiconósico"\
+                    ,"linfogranulomatosis inginalis"\
+                    ,"narainkarthikeyanismo"\
+                    ,"dimetilnitrosamina"]
+
+    medium_words = ["bosniaca"\
+                    ,"bosniaco"\
+                    ,"bosniaca"\
+                    ,"bosniaco"\
+                    ,"botanica"\
+                    ,"bramador"\
+                    ,"bretonas"\
+                    ,"bretones"\
+                    ,"bridones"\
+                    ,"brumador"\
+                    ,"brujulas"\
+                    ,"bulgaras"\
+                    ,"bulgaros"\
+                    ,"cinetico"\
+                    ,"cisteína"\
+                    ,"coaligas"\
+                    ,"cobrizas"]
+
+    easy_words = ["mala"\
+                ,"mece"\
+                ,"medi"\
+                ,"trae"\
+                ,"tuve"\
+                ,"orzo"\
+                ,"osas"\
+                ,"osea"\
+                ,"osos"\
+                ,"dañe"\
+                ,"dome"]
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((HOST,PORT))
@@ -25,15 +74,38 @@ if __name__ == "__main__":
             while True:
 
                 data = client_s.recv(1024)
+
+                #If there is not response from the client then we shutdown
+                #the communication with that client
                 if not data:
                     print(f"Connection finished with: {client_s.getpeername()[0]} : {client_s.getpeername()[1]}")
                     break
 
                 #We also need to cast the data to int
                 data = int(data)
+                choosen_word = ''
+                #Request to choose a difficulty      
                 if(data==1):
-                    print(1)
-            
-            
+                    print("Request to choose a difficulty has been received")
+                    difficulty = client_s.recv(1024).decode()
+                   
+                    if(difficulty=='easy'):
+                        print(" easy diffilculty chosen")
+                        choosen_word = random.choice(easy_words)
+                        len_choosen_word = str(len(choosen_word)).encode()
+                        client_s.send(len_choosen_word)
+                        time.sleep(0.05)
 
-        
+                    elif(difficulty=='medi'):
+                        print(" medium diffilculty chosen")
+                        choosen_word = random.choice(medium_words)
+                        len_choosen_word = str(len(choosen_word)).encode()
+                        client_s.send(len_choosen_word)
+                        time.sleep(0.05)
+
+                    elif(difficulty=='hard'):
+                        print(" hard diffilculty chosen")
+                        choosen_word = random.choice(hard_words)
+                        len_choosen_word = str(len(choosen_word)).encode()
+                        client_s.send(len_choosen_word)
+                        time.sleep(0.05)
